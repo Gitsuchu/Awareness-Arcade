b7256a038e47faa5ce4aed343e1734839abb9f5b
Suchi chawra <129256361+Gitsuchu@users.noreply.github.com>
2024-03-11 23:34:46 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Did you know what's Age limit for CHILD LABOUR?",
+    answer: "Greater than 14 years",
+    options: [
+      "less than 10 years",
+      "only age of 12years",
+      "Greater than 14 years",
+      "Age of 21 years"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In which of the following areas is child labour found in abundance?",
+    answer: "All of the above",
+    options: [
+      "Hotel industry",
+      "Tabacco,Matchsticks making and fireworks factory",
+      "carpet weaving and diamond polishing",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What the major reason for child labour?",
+    answer: "Poverty and less population",
+    options: [
+      "For their enjoyment",
+      "For playing games",
+      "Poverty and less population",
+      "Studing less and going out"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Sara is harmed by others.What is the right that can protect her from harm and voilence ?",
+    answer: "The right to protection",
+    options: [
+      "The right to eat",
+      "The right to protection",
+      "The right to speak",
+      "The right to action"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Suppose someone is force you to work with them.What would to say ?",
+    answer: "Don't force me,it is illegal",
+    options: [
+      "sure, I'm ready",
+      "Wait,I'll ask my friends too",
+      "Don't force me,it is illegal",
+      "I'll come after my school hours"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
